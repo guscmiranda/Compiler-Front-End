ba4311b1a7fbb29e39292d943ab3489317b16241
guscmiranda <gcmiranda2005@gmail.com>
2026-06-11 23:06:10 -0300

    feat: Contagem de linhas e colunas, tratamento do lookahed, verificação de eof na get_next_token

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -1,5 +1,3 @@
-#include "lexer.h"
-#include "tabela_transicao.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -7,71 +5,95 @@
 #include "tabela_transicao.h"
 #include "../utils/error.h"
 #include "lexer.h"
-// #include "token.h"
 
 int linha_atual = 1;
 int coluna_atual = 1;
+int coluna_anterior = 1;
 FILE *arquivo = NULL;
 
 extern Estado tabela_transicao[N_ESTADOS][NUM_CLASSES];
 extern TokenType estado_final[N_ESTADOS];
 extern int estado_lookahead[N_ESTADOS];
 
-void inicializa_lexer(FILE *codigo_fonte) {
+void inicializa_lexer(FILE *codigo_fonte)
+{
     arquivo = codigo_fonte;
     inicializa_tabela_transicao();
-    linha_atual = 1;
-    coluna_atual = 1;
 }
 
-// falta get next token no lexer.c, tem que ler o arquivo e usar a tabela de transicao pra gerar os 
+// falta get next token no lexer.c, tem que ler o arquivo e usar a tabela de transicao pra gerar os
 // tokens e usar a funcao verifica_palavra_reservada pra diferenciar id de palavra reservada
 
-static TokenType verifica_palavra_reservada(const char* lexema) {
-    //tipo
-    if (strcmp(lexema, "void") == 0) return TK_TIPO;
-    if (strcmp(lexema, "int") == 0) return TK_TIPO;
-    if (strcmp(lexema, "char") == 0) return TK_TIPO;
-    if (strcmp(lexema, "float") == 0) return TK_TIPO;
-    
-    //estrutura
-    if (strcmp(lexema, "programa") == 0) return TK_PROGRAMA;
-    if (strcmp(lexema, "inicio") == 0) return TK_INICIO;
-    if (strcmp(lexema, "fim") == 0) return TK_FIM;
+static TokenType verifica_palavra_reservada(const char *lexema)
+{
+    // tipo
+    if (strcmp(lexema, "void") == 0)
+        return TK_TIPO;
+    if (strcmp(lexema, "int") == 0)
+        return TK_TIPO;
+    if (strcmp(lexema, "char") == 0)
+        return TK_TIPO;
+    if (strcmp(lexema, "float") == 0)
+        return TK_TIPO;
 
-    //selecao
-    if (strcmp(lexema, "se") == 0) return TK_SE;
-    if (strcmp(lexema, "entao") == 0) return TK_ENTAO;
-    if (strcmp(lexema, "e_se") == 0) return TK_E_SE;
-    if (strcmp(lexema, "senao") == 0) return TK_SENAO;
+    // estrutura
+    if (strcmp(lexema, "programa") == 0)
+        return TK_PROGRAMA;
+    if (strcmp(lexema, "inicio") == 0)
+        return TK_INICIO;
+    if (strcmp(lexema, "fim") == 0)
+        return TK_FIM;
 
-    //repeticao
-    if (strcmp(lexema, "enquanto") == 0) return TK_ENQUANTO;
-    if (strcmp(lexema, "faca") == 0) return TK_FACA;
-    if (strcmp(lexema, "repita") == 0) return TK_REPITA;
-    if (strcmp(lexema, "ate") == 0) return TK_ATE;
-    if (strcmp(lexema, "para") == 0) return TK_PARA;
-    if (strcmp(lexema, "passo") == 0) return TK_PASSO;
+    // selecao
+    if (strcmp(lexema, "se") == 0)
+        return TK_SE;
+    if (strcmp(lexema, "entao") == 0)
+        return TK_ENTAO;
+    if (strcmp(lexema, "e_se") == 0)
+        return TK_E_SE;
+    if (strcmp(lexema, "senao") == 0)
+        return TK_SENAO;
 
-    //se passa eh id
+    // repeticao
+    if (strcmp(lexema, "enquanto") == 0)
+        return TK_ENQUANTO;
+    if (strcmp(lexema, "faca") == 0)
+        return TK_FACA;
+    if (strcmp(lexema, "repita") == 0)
+        return TK_REPITA;
+    if (strcmp(lexema, "ate") == 0)
+        return TK_ATE;
+    if (strcmp(lexema, "para") == 0)
+        return TK_PARA;
+    if (strcmp(lexema, "passo") == 0)
+        return TK_PASSO;
+
+    // se passa eh id
     return TK_ID;
 }
 
-void conta_linha_e_coluna(char c){
+void conta_linha_e_coluna(char c)
+{
     const int is_eof = (c == EOF);
 
-    if (c == '\n'){
+    if (c == '\n')
+    {
         linha_atual++;
+        coluna_anterior = coluna_atual;
         coluna_atual = 1;
-    } else if (!is_eof){
+    }
+    else if (!is_eof)
+    {
         coluna_atual++;
     }
 }
 
-void trata_lookahead(Estado s, char *lexema, int *tamanho_lexema){
-    
+void trata_lookahead(Estado s, char *lexema, int *tamanho_lexema)
+{
+
     // Se alcançou s sem lookahead não precisa tratar
-    if(estado_lookahead[s] != 1) return;
+    if (estado_lookahead[s] != 1)
+        return;
 
     (*tamanho_lexema)--;
     char lookahead_char = lexema[*tamanho_lexema];
@@ -79,14 +101,19 @@ void trata_lookahead(Estado s, char *lexema, int *tamanho_lexema){
     // Devolve caractere e volta o ponteiro (conferir!)
     ungetc(lookahead_char, arquivo);
 
-    if (lookahead_char == '\n') linha_atual--;
-    else coluna_atual--;
-
+    if (lookahead_char == '\n')
+    {
+        linha_atual--;
+        coluna_atual = coluna_anterior;
+    }
+    else
+        coluna_atual--;
 }
 
-int get_next_token(){
+Token get_next_token()
+{
     Estado s = estado_inicial();
-    char lexema[100]; 
+    char lexema[100] = {0}; // buffer para construir o lexema
     int i = 0;
 
     // Para reportar erro, informar onde o token está exatamente
@@ -99,112 +126,87 @@ int get_next_token(){
         int is_eof = (c == EOF);
 
         conta_linha_e_coluna(c); // Atualizar linha e coluna
-        if(!is_eof && c != '\r') lexema[i++] = c;
+        // if (!is_eof && c != '\r')
+        lexema[i++] = c;
 
-        if(is_eof && s == estado_inicial()){
-            // EOF REAL
-            // CRIAR TOKEN NULO ?
-            // SAIR DAQUI E IR PRA AÇÕES E TRATAR LÁ?
-        } 
+        if (is_eof /*&& s == estado_inicial()*/)
+        {
+            if (s == estado_inicial())
+            {
+                return (Token){.tipo = TK_EOF, .atributo = "\0", .linha = linha_atual, .coluna = coluna_atual + 1};
+            }
+            else
+            {
+                c = ' ';
+            }
+        }
         // se o eof é lookahead temos q tratar num else ??
-        
 
         ClasseEntrada classe = classifica_caractere(c);
         s = move(s, classe);
-        
-    
     }
 
-    // duas linhas abaixo apenas para testes
-    trata_lookahead(s, lexema, &i);
-    lexema[i] = '\0';
+    // return acoes(s, lexema, token_linha, token_coluna);
+    Token token_atual = acoes(s, lexema, token_linha, token_coluna);
 
+    if (token_atual.tipo == TK_SEPARADOR || token_atual.tipo == TK_COMENTARIO)
+    {
+        // repetir, talvez devesemos colocar tudo dentro de outro while
+        // aí colocava um continue;
+    }
 
-    printf("lexema lido: %s | Token: %d\n", lexema, token_final(s));
-    int token = token_final(s);
-    // acoes(lexema, token);
+    return token_atual;
 }
 
 // Considera que EOF é tratado no while
 // Acho que não trata RELOP de acordo...
-// Deveria retornar lexema nos q não são id ?
-Token acoes(Estado s, char *lexema, int *tamanho_lexema, 
-int token_linha, int token_coluna){
+Token acoes(Estado s, char *lexema, int token_linha, int token_coluna)
+{
 
     Token token;
 
-    if (s == ST_ERRO){ // Conferir
-        lexema[*tamanho_lexema] = '\0';
+    int tamanho_lexema = strlen(lexema);
+
+    if (s == ST_ERRO)
+    { // Conferir
+        lexema[tamanho_lexema] = '\0';
         // chama função de erro
         token.tipo = TK_NULO;
+        token.linha = token_linha;
+        token.coluna = token_coluna;
         return token;
     }
 
-    trata_lookahead(s, lexema, tamanho_lexema);
-    lexema[*tamanho_lexema] = '\0';
+    trata_lookahead(s, lexema, &tamanho_lexema);
+    lexema[tamanho_lexema] = '\0';
 
     TokenType tipo = estado_final[s];
 
-    if(tipo == TK_ID)
+    if (tipo == TK_ID)
+    {
         tipo = verifica_palavra_reservada(lexema);
+        if (tipo == TK_ID)
+        { // se for id, tem que verificar se o lexema pertence à tabela de símbolos
+            token.atributo[0] = 48 + indice_simbolo(lexema);
+            token.atributo[1] = '\0';
+        }
+    }
+    else if (tipo == TK_RELOP)
+    {
+        // Para relop, o atributo é o próprio lexema (==, !=, <, >, <=, >=)
+        strcpy(token.atributo, lexema);
+    }
+    else
+    {
+        // Para outros tokens, o atributo é o próprio lexema (ex: número, char)
+        strcpy(token.atributo, "\0");
+    }
+
+    // strcpy(token.atributo, lexema);
 
     token.tipo = tipo;
-    strcpy(token.lexema, lexema);
     token.linha = token_linha;
     token.coluna = token_coluna;
 
-    if(tipo == TK_ID || tipo == TK_NUM){
-        // adiciona na tabela de simbolos
-        // indice = insere_simbolo(token.lexema, tipo) ??
-        token.tipo = -1; // só pra não reclamar
-    } else {
-        token.indice = -1;
-    }
-
     return token;
 }
-
-// int acoes(lexema, id_token) {
-//     switch (id_token) {
-//         case TK_ID:
-//         // tabela de simbolos
-//         // lookahead
-//             verifica_palavra_reservada(lexema);
-//         default:
-//             return id_token;
-//     }
-//     TK_NULO, // 0
-//     TK_TIPO,
-//     TK_PROGRAMA,
-//     TK_ID,
-//     TK_PARENTESE_ABRE,
-//     TK_PARENTESE_FECHA, // 5
-//     TK_INICIO,
-//     TK_FIM,
-//     TK_DOIS_PONTOS,
-//     TK_PONTO_VIRGULA,
-//     TK_VIRGULA, // 10
-//     TK_ATRIBUICAO,
-//     TK_OP_ADI,
-//     TK_OP_SUB,
-//     TK_OP_MUL,
-//     TK_OP_DIV, // 15
-//     TK_OP_POT,
-//     TK_CHAR_CONTEUDO,
-//     TK_NUM,
-//     TK_SE,
-//     TK_ENTAO, // 20
-//     TK_E_SE,
-//     TK_SENAO,
-//     TK_ENQUANTO,
-//     TK_FACA,
-//     TK_REPITA, // 25
-//     TK_ATE,
-//     TK_RELOP,
-//     TK_PARA,
-//     TK_PASSO,
-//     TK_COMENTARIO, // 30
-//     TK_SEPARADOR,  // comentário e separador são ignorados
-//     TK_EOF,
-//     NUM_TOKENS
-// }
--- lexer/lexer.h
+++ lexer/lexer.h
@@ -10,9 +10,8 @@
 // Token get_next_token();
 
 void inicializa_lexer(FILE *codigo_fonte);
-int get_next_token();
-Token acoes(Estado s, char *lexema, int *tamanho_lexema, int token_linha, int token_coluna);
+Token get_next_token();
+Token acoes(Estado s, char *lexema, int token_linha, int token_coluna);
 // Token acao(char *lexema, int );
-
 
 #endif // LEXER_H
