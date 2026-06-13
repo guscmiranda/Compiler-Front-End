0b413781bb74705916ed425623c9fd3a08ff9db7
Daniel Borges <44danbor@gmail.com>
2026-06-13 17:16:17 -0300

    feature: adiciona alocação dinâmica para o vetor de tokens lidos

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "lexer/tabela_transicao.h"
 #include "symbols/tabela_simbolos.h"
 #include "lexer/lexer.h"
+#include <stdlib.h>
 #include <stdio.h>
 
 int main()
@@ -13,36 +14,44 @@ int main()
     }
     else
     {
-        printf("Arquivo lido com sucesso\n");
+        printf("Arquivo lido com sucesso\n\n");
     }
 
-    Token tokens[500]; // Array para armazenar os tokens gerados
+    int capacidade = 100;
+    int quantidade = 0;
+    Token *tokens = malloc(capacidade * sizeof(Token));
 
     inicializa_lexer(arquivo);
     int i = 0;
     do
     {
-        tokens[i] = get_next_token();
-        if (tokens[i].tipo == TK_NULO)
+        if (quantidade == capacidade)
         {
-            printf("Token de erro encontrado em linha %d, coluna %d.\n", tokens[i].linha, tokens[i].coluna);
-            break;
+            capacidade += 100;
+            Token *temp = realloc(tokens, capacidade * sizeof(Token));
+            if (temp == NULL)
+            {
+                fprintf(stderr, "Erro de realocação de memoria.\n");
+                free(tokens);
+                fclose(arquivo);
+                return 1;
+            }
+            tokens = temp;
         }
-        else
-        {
-            // printf("leu com i = %d", i);
-            // printf("Token: %-5d| Atributo: %-5s| Linha: %-3d| Coluna: %-3d\n", tokens[i].tipo, tokens[i].atributo, tokens[i].linha, tokens[i].coluna);
-        }
-        i++;
-    } while (tokens[i - 1].tipo != TK_EOF);
 
-    for (int j = 0; j < i; j++)
+        tokens[quantidade] = get_next_token();
+
+        quantidade++;
+    } while (tokens[quantidade - 1].tipo != TK_EOF);
+
+    for (int j = 0; j < quantidade; j++)
     {
         printf("Token: %-5d | Atributo: %-5s | Linha: %-3d | Coluna: %-3d\n", tokens[j].tipo, tokens[j].atributo, tokens[j].linha, tokens[j].coluna);
     }
 
     print_symbol_table();
 
+    free(tokens);
     fclose(arquivo);
 
     return 0;
