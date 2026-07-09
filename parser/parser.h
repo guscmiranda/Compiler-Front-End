1b0ba1fb8ddeb9c076daf75e61115f453def5d7b
guscmiranda <gcmiranda2005@gmail.com>
2026-07-09 01:39:48 -0300

    Reorganização do .h

--- parser/parser.h
+++ parser/parser.h
@@ -4,29 +4,35 @@
 #include "arvore.h"
 #include "../lexer/lexer.h"
 
-//alguem confirma se ta tudo aqui tenho medo de pular
-enum TipoToken {
-    TK_TIPO, TK_PROGRAMA, TK_PARENTESE_ABRE, TK_PARENTESE_FECHA,
-    TK_INICIO, TK_FIM, TK_ID, TK_NUM, TK_CHAR_CONTEUDO,
-    TK_OP_ADI, TK_OP_SUB, TK_OP_MUL, TK_OP_DIV, TK_OP_POT,
-    TK_ATRIBUICAO, TK_DOIS_PONTOS, TK_PONTO_VIRGULA, TK_VIRGULA,
-    TK_SE, TK_ENTAO, TK_E_SE, TK_SENAO, TK_ENQUANTO, TK_FACA,
-    TK_REPITA, TK_ATE, TK_PARA, TK_PASSO, TK_RELOP, TK_EOF
-};
-
-extern Token tk_atual;
-
-Token get_next_token(); 
-void match(int token_esperado);
-void erro_sintatico(char *mensagem_esperada);
+No *parser();
+void match(TokenType token_esperado);
 
 // producoes da LL1
 No *call();
 No *bloco();
-No *termo();
 No *declaracao_das_variaveis();
+No *lista_ids();
+No *lista_ids_linha();
 No *sequencia_de_comandos();
+No *comando_atribuicao();
+No *expressao();
+No *expressao_linha();
+No *expressao2();
+No *expressao2_linha();
 No *expressao3();
 No *expressao3_linha();
+No *termo();
+No *num_ou_id();
+No *comando();
+No *comando_ou_bloco();
+No *comando_condicional();
+No *condicao();
+No *parte_e_se();
+No *parte_senao();
+No *comando_enquanto();
+No *comando_repita();
+No *comando_para();
+No *op_adi_ou_sub();
+No *op_mul_ou_div();
 //...
-#endif 
+#endif
